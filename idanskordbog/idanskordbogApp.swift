0f0d43091a6ac9867c0b19703aa7513027d0e50f
Duc <dtngyn42@gmail.com>
2022-03-11 23:21:30 +0100

    Initial Commit

---
+++ idanskordbog/idanskordbogApp.swift
@@ -0,0 +1,17 @@
+//
+//  idanskordbogApp.swift
+//  idanskordbog
+//
+//  Created by Amelia on 11.03.22.
+//
+
+import SwiftUI
+
+@main
+struct idanskordbogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
